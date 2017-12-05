43748773a7e68d06062eb66ae6cceabbf62600a0
Walter Renner <renner@arteria.ch>
2017-12-05 09:55:34 +0100

    use namespaces urls

--- hijack_admin/tests/urls.py
+++ hijack_admin/tests/urls.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = [
-    url(r'^hijack/', include('hijack.urls')),
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^hello/', include('hijack.tests.test_app.urls'))
+    url(r'^admin/', admin.site.urls),
+    url(r'^hijack/', include('hijack.urls', namespace='hijack')),
+    url(r'^hello/', include('hijack.tests.test_app.urls', namespace='test_app'))
 ]
